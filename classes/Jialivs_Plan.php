8a16ca8f3e7a90e3a17fad524aba32d1c7065d28
jiali <mahyarerad@gmail.com>
2025-05-04 23:32:47 +0330

    making plan class

--- classes/Jialivs_Plan.php
+++ classes/Jialivs_Plan.php
@@ -19,4 +19,41 @@ class Jialivs_Plan {
         return $stmt;
     }
 
+    public function find_by_id($plan_id) {
+        $stmt = $this->db->get_row($this->db->prepare("SELECT * FROM {$this->vipTable} WHERE id = %d", $plan_id));
+        return $stmt;
+    }
+
+    public static function get_plan_title($plan_id) {
+        $vip_plan_title = '';
+        switch($plan_id) {
+            case 1:
+                $vip_plan_title = 'پکیج طلایی';
+                break;
+            case 2:
+                $vip_plan_title = 'پکیج نقره ای';
+                break;
+            case 3:
+                $vip_plan_title = 'پکیج برنزی';
+                break;
+        }
+        return $vip_plan_title;
+    }
+
+    public static function get_plan_icon($plan_id) {
+        $vip_plan_icon = 'lni-layers';
+        switch($plan_id) {
+            case 1:
+                $vip_plan_icon = 'lni-layers';
+                break;
+            case 2:
+                $vip_plan_icon = 'lni-diamond';
+                break;
+            case 3:
+                $vip_plan_icon = 'lni-invention';
+                break;
+        }
+        return $vip_plan_icon;
+    }
+
 }
