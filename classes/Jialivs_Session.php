ddfbb455491bd587d341a538b0092a061b7d05e5
jiali <mahyarerad@gmail.com>
2025-05-04 23:32:56 +0330

    making session class

---
+++ classes/Jialivs_Session.php
@@ -0,0 +1,26 @@
+<?php 
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) exit;
+
+class Jialivs_Session {
+
+    public static function has( $key ) {
+        return array_key_exists( $key, $_SESSION );
+    }
+
+    public static function set( $key, $value ) {
+        $_SESSION[$key] = $value;
+    }
+
+    public static function get( $key ) {
+        if( self::has($key) )
+            return $_SESSION[$key];
+    }
+
+    public static function unset( $key ) {
+        if( self::has($key) )
+            unset($_SESSION[$key]);
+    }
+
+}
