b812680eed8babf5936b803331a42498004af37c
jiali <mahyarerad@gmail.com>
2025-05-09 15:39:47 +0330

    Adding filter to vip posts

--- _inc/filter-vip-content.php
+++ _inc/filter-vip-content.php
@@ -3,10 +3,10 @@
 // Exit if accessed directly
 if (!defined('ABSPATH')) exit;
 
-// Register Meta Box
+// Add filter
 function jialivs_filter_vip_content( $content ) {
     $user_vip_plan = new Jialivs_User_Vip_Plan();
-    if( !$user_vip_plan->is_user_vip( get_current_user_id(  ) ) ) {
+    if( is_single(  ) && !$user_vip_plan->is_user_vip( get_current_user_id(  ) ) ) {
         return mb_substr($content, 0, 500, 'UTF-8' ) . '...' . '<div class="alert alert-danger">برای مطالعه ادامه مطلب، اکانت vip تهیه نمایید</div>';
     }
     return $content;
