67e72c5ce57cd5419a255d9cee4bac4e733b15a0
jiali <mahyarerad@gmail.com>
2025-05-11 10:06:16 +0330

    Creating flash message class

---
+++ classes/Jialivs_Flash_Message.php
@@ -0,0 +1,35 @@
+<?php
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) exit;
+
+class Jialivs_Flash_Message {
+
+    const ERROR = 0;
+    const SUCCESS = 1;
+
+    public static function addMessage( $message = null, $type ) {
+        if( isset($_SESSION['flash_message']) ) {
+            $_SESSION['flash_message'] = [
+                'message' => $message,
+                'type' => $type
+            ];
+        } else {
+            $_SESSION['flash_message'] = [];
+            $_SESSION['flash_message'] = [
+                'message' => $message,
+                'type' => $type
+            ];
+        }
+    }
+    
+    public static function showMessage(  ) {
+        if( isset($_SESSION['flash_message']) ): ?>
+            <div class="<?php echo $_SESSION['flash_message']['type'] == self::SUCCESS ? 'uk-alert-success' : 'uk-alert-danger' ?>" uk-alert>
+                <p><?php echo $_SESSION['flash_message']['message'] ?></p>
+            </div>
+        <? 
+            unset($_SESSION['flash_message']);
+        endif;
+    }
+}
